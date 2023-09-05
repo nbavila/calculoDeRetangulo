f4d8b095bfb8d328b6e6c01bf78746ab813c3c0b
Lucas Ávila <avilalucas11@gmail.com>
2023-09-04 23:25:33 -0300

    Atualizando projeto

--- estudoEmPOO/src/application/Program.java
+++ estudoEmPOO/src/application/Program.java
@@ -9,7 +9,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		;
+
 		Scanner sc = new Scanner(System.in);
 		Rectangle total = new Rectangle();
 
